f88dcf26778ac344b692c79a29c824aacfb3762c
HIMZ <himz5221@naver.com>
2023-03-16 16:28:51 +0900

    ㅎㅎ..

---
+++ TeamProject/src/main/java/co/prod/service/HotelFilterServiceImpl.java
@@ -0,0 +1,25 @@
+package co.prod.service;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import co.prod.common.DataSource;
+import co.prod.mapper.HotelFilterMapper;
+import co.prod.vo.HotelsVO;
+
+public class HotelFilterServiceImpl implements HotelFilterService{
+	SqlSession sqlSession = DataSource.getInstance().openSession(true);
+	HotelFilterMapper mapper = sqlSession.getMapper(HotelFilterMapper.class);
+	
+	@Override
+	public List<HotelsVO> getHotelList() {
+		List<HotelsVO> list = mapper.getHotelList();
+		for(HotelsVO i : list) {
+			System.out.println(i);
+		}
+		// TODO Auto-generated method stub
+		return list;
+	}
+
+}
