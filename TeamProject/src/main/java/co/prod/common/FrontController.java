35a41ea4f2abce45053ecf74c8808cec673ec36e
HIMZ <himz5221@naver.com>
2023-03-16 17:47:07 +0900

    김인규 20230316

--- TeamProject/src/main/java/co/prod/common/FrontController.java
+++ TeamProject/src/main/java/co/prod/common/FrontController.java
@@ -11,9 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import co.prod.controller.MainPageListControl;
-import co.prod.controller.ManageHotelAddControl;
+//import co.prod.controller.ManageHotelAddControl;
 import co.prod.controller.ManageHotelListControl;
-import co.prod.controller.PostListControl;
 import co.prod.controller.posts.PostListControl;
 import co.prod.controller.posts.PostWriteControl;
 import co.prod.controller.posts.PostWriteFormControl;
@@ -33,7 +32,7 @@ public class FrontController extends HttpServlet{
 		map.put("/postList.do", new PostListControl());
 		map.put("/mainpage.do", new MainPageListControl());
 		map.put("/manageHotelList.do", new ManageHotelListControl());
-		map.put("/manageHotelAdd.do", new ManageHotelAddControl());
+		//map.put("/manageHotelAdd.do", new ManageHotelAddControl());
 		map.put("/viewPost.do", new ViewPostControl());
 		map.put("/postWriteForm.do", new PostWriteFormControl());
 		map.put("/postWrite.do", new PostWriteControl());
