d3965919e3ed1f97889f85efaa92c331eb31f52f
Seonjae Kim <ksj9509180@gmail.com>
2023-03-24 09:15:33 +0900

    333

--- TeamProject/src/main/java/co/prod/control/LoginControl.java
+++ TeamProject/src/main/java/co/prod/control/LoginControl.java
@@ -30,8 +30,6 @@ public class LoginControl implements Control {
 		System.out.println(vo);
 		
 		HttpSession session = request.getSession();
-		session.setAttribute("email", vo.getUserEmail());
-		session.setAttribute("userVO", vo);
 		
 		if(vo!=null) {
 			
